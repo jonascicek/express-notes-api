d8b6d065f1a9e8b6d41ba5d694ff9eeb4c927047
jonascicek <jonascicek98@gmail.com>
2025-01-15 16:12:36 +0100

    Debugged delete const port env...

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
-
+const port = process.env.NOTES_API_PORT || 8080;
 
 
 app.listen(port, () => {
@@ -9,8 +8,10 @@ app.listen(port, () => {
 });
 
 let notes = [
-    { note: 'My new Note', autor: 'Max Mustermann', date: '2025-01-15' },
+    { id: 1, note: 'My new Note', autor: 'Max Mustermann', date: '2025-01-15' }
 ];
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
     response.send('Hello World');
@@ -18,6 +19,40 @@ app.get('/', (request, response) => {
 
 app.get('/notes', (request, response) => {
     response.json(notes);
+});
+
+app.get('/notes/:id', (request, response) => {
+    const id = request.params.id;
+    const note = notes[id];
+    response.json(note);
+});
+
+app.put('/notes/:id', (request, response) => {
+    const id = request.params.id;
+    const note = notes[id];
+    note.note = request.body.note;
+    response.json(note);
+});
+
+app.post('/notes', (request, response) => {
+    const newNote = {
+        id: notes.length + 1,
+        note: request.body.note,
+        autor: request.body.autor,
+        date: request.body.date,
+    };
+    notes.push(newNote);
+    response.json(notes);
+});
+
+app.delete('/notes/:id', (request, response) => {
+    const id = request.params.id;
+    notes = notes.filter((note) => note.id !== id);
+
+    if (notes === -1) {
+        response.status(404).send('Note not found');
+    }
+    response.send("Note deleted");
 });
 
 
